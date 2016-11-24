e49d6a8b1c300feb4cded4d29e3afc9152240192
Andreas Pung <andreaspung@gmail.com>
2016-11-24 11:33:33 +0200

    Added 'times' list and refactoring
    Refactored 'displayTime' function and started building 'times' list,
    fixed timer rounding issue (sometimes displayed 0.6 instead of 0.60),
    changed solveTime variable name.

--- Main.py
+++ Main.py
@@ -13,35 +13,41 @@ pygame.display.set_caption("Ajapinu")
 clock=pygame.time.Clock()
 appDisplay.fill(WHITE)
 
-text = "0.00"
-timeFont = pygame.font.SysFont("Arial", 72)
-textPicture = timeFont.render(text, False, BLACK)
-appDisplay.blit(textPicture, (displayWidth/2-72, displayHeight/2-72))
-
-def displayTime(textPicture):
-    textPicture=timeFont.render(text, False, BLACK)
+def displayTime(text):
+    text=str(text)
+    timeFont = pygame.font.SysFont("Arial", 72)
+    textPicture = timeFont.render(text, False, BLACK)
     appDisplay.blit(textPicture, (displayWidth/2-72, displayHeight/2-72))
 
+solveTime="0.00"
+displayTime(solveTime)
 
-beginTime=0
+
 timerRunning=False
+beginTime=0
+times=[]
+solves=0
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             break
         if event.type == pygame.KEYDOWN:
             if event.key==pygame.K_SPACE and not timerRunning:
-                print("YES")
                 beginTime=time()
                 timerRunning=True
+                solves+=1
             elif event.key==pygame.K_SPACE and timerRunning:
                 timerRunning=False
 
     if timerRunning:
-        text=str(round(time()-beginTime, 2))
+        solveTime="%.2f" % round(time()-beginTime, 2)
+
+    if not timerRunning and beginTime!=0:
+        if len(times)!=solves:
+            times.append(float(solveTime))
     
     appDisplay.fill(WHITE)
-    displayTime(textPicture)
+    displayTime(solveTime)
     pygame.display.update()
     clock.tick(60)
 
